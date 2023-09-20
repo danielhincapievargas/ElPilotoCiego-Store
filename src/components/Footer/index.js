d58ab49b7d7e47f239d2643bbf8ab65641df24b0
danielhincapievargas <daniel.hincapie.vargas@gmail.com>
2023-09-20 16:21:08 -0500

    footer init

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import styles from '@components/Footer/footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <h1>Footer</h1>
+    <div className={styles.footer_container}>
+      <div className={styles.footer_logo}>
+        <Image 
+        className={styles.logo}
+        src="/elpiloto-logo.png"
+        alt="El Piloto Ciego Logo"
+        width={150}
+        height={150}
+        priority
+        />
+      </div>
+      <div className={styles.footer_section}>
+        <ul>
+          <li>Home</li>
+          <li>Search</li>
+          <li>FAQ</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.footer_section}>
+        <ul>
+          <li>Apparel</li>
+          <li>Accesories</li>
+          <li>Music</li>
+          <li>About</li>
+        </ul>
+      </div>
+      <div className={styles.footer_section}>
+        <ul>
+          <li>Privacy Policy</li>
+          <li>Terms of Service</li>
+          <li>Do not sell or share my personal information</li>
+        </ul>
+      </div>
+
+    </div>
   )
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <>
       <Layout>
-        <div>Content</div>
+        <di>Content</di>
       </Layout>
     </>
   )
