5d6e47d75b203e9081dda0235e81cb3e0a8081ad
danielhincapievargas <daniel.hincapie.vargas@gmail.com>
2023-09-20 22:19:54 -0500

    Footer done

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,44 +1,58 @@
 import React from 'react'
 import styles from '@components/Footer/footer.module.css'
 import Image from 'next/image'
+import { TfiEmail } from "react-icons/tfi";
 
 const Footer = () => {
   return (
     <div className={styles.footer_container}>
-      <div className={styles.footer_logo}>
-        <Image 
-        className={styles.logo}
-        src="/elpiloto-logo.png"
-        alt="El Piloto Ciego Logo"
-        width={150}
-        height={150}
-        priority
-        />
+      <div className={styles.footer}>
+        <div className={styles.footer_left}>
+          <div className={styles.footer_logo}>
+            <Image 
+            className={styles.logo}
+            src="/elpiloto-logo.png"
+            alt="El Piloto Ciego Logo"
+            width={150}
+            height={150}
+            priority
+            />
+          </div>
+          <div className={styles.footer_section}>
+            <ul>
+              <li>Home</li>
+              <li>Search</li>
+              <li>FAQ</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div className={styles.footer_section}>
+            <ul>
+              <li>Apparel</li>
+              <li>Accesories</li>
+              <li>Music</li>
+              <li>About</li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.footer_right}>
+          <div className={styles.footer_section}>
+            <ul>
+              <li>Privacy Policy</li>
+              <li>Terms of Service</li>
+            </ul>
+          </div>
+          <div className={styles.footer_newsletter}>
+            <h5>NEWSLETTER</h5>
+            <p>Suscribe to stay in the loop about music, merch, shows, and more.</p>
+            <div className={styles.email_container}>
+              <input type="email" placeholder="Enter your email" />
+              <TfiEmail className={styles.email_icon}/>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className={styles.footer_section}>
-        <ul>
-          <li>Home</li>
-          <li>Search</li>
-          <li>FAQ</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-      <div className={styles.footer_section}>
-        <ul>
-          <li>Apparel</li>
-          <li>Accesories</li>
-          <li>Music</li>
-          <li>About</li>
-        </ul>
-      </div>
-      <div className={styles.footer_section}>
-        <ul>
-          <li>Privacy Policy</li>
-          <li>Terms of Service</li>
-          <li>Do not sell or share my personal information</li>
-        </ul>
-      </div>
-
+        <p className={styles.made_by}>This website was made by Daniel Hincapié Vargas</p>
     </div>
   )
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Bangers&family=DM+Sans:wght@400;500;700&family=Fraunces:opsz@9..144&family=Inter:wght@300;500&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;0,1000;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900;1,1000&family=Oswald:wght@200;300;400;500;600;700&family=Outfit:wght@400;700&family=Overpass:wght@400;700&family=Poppins:wght@500&family=Quicksand:wght@300;400;500;600;700&family=Roboto+Mono:ital,wght@0,700;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Space+Mono:wght@400;700&family=Special+Elite&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
       </Head>
       <body>
