613d55daac94a1a3d1ebb3646b128043a2c50135
Daniel Hincapié Vargas <118952312+danielhincapievargas@users.noreply.github.com>
2023-09-20 22:49:53 -0500

    Feature/footer (#2)
    * footer init
    
    * Footer done
    
    * footer position

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,7 +6,38 @@ const Home = () => {
   return (
     <>
       <Layout>
+<<<<<<< HEAD
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        <h1>ContentContentContentContentContentContentContentContentContentContent</h1>
+        
+=======
         <di>Content</di>
+>>>>>>> 1372b56198d71a20f59d7832b5b7ba54acaf62e5
       </Layout>
     </>
   )
