67de0c981d4fcb465b370dee5deb61bd1eb5e79b
Daniel Hincapié Vargas <118952312+danielhincapievargas@users.noreply.github.com>
2023-09-24 18:00:40 -0500

    product detail page done (#7)

---
+++ src/pages/product_detail.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '@/components/Layout'
+import ProductDetail from '@/components/ProductDetail'
+
+const ProductDetailPage = () => {
+  return (
+    <Layout>
+      <ProductDetail />
+    </Layout>
+  )
+}
+
+export default ProductDetailPage
