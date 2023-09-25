c84b010c7c7e7b8334639cf4d97666a482cb995f
danielhincapievargas <daniel.hincapie.vargas@gmail.com>
2023-09-25 11:03:55 -0500

    apparel, accesories, music pages done

---
+++ src/pages/music.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '@/components/Layout'
+import ProductList from '@/components/ProductList'
+
+const MusicPage = () => {
+  return (
+    <Layout>
+      <ProductList title={'MUSIC'}/>
+    </Layout>
+  )
+}
+
+export default MusicPage
