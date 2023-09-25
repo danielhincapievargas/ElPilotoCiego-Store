38a092fc4bb4c933c6e242c2cb92b105c3466ac7
Daniel Hincapié Vargas <118952312+danielhincapievargas@users.noreply.github.com>
2023-09-25 11:05:36 -0500

    apparel, accesories, music pages done (#11)

---
+++ src/pages/accesories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '@/components/Layout'
+import ProductList from '@/components/ProductList'
+
+const AccesoriesPage = () => {
+  return (
+    <Layout>
+      <ProductList title={'ACCESORIES'}/>
+    </Layout>
+  )
+}
+
+export default AccesoriesPage
---
+++ src/pages/apparel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '@/components/Layout'
+import ProductList from '@/components/ProductList'
+
+const ApparelPage = () => {
+  return (
+    <Layout>
+      <ProductList title={'APPAREL'}/>
+    </Layout>
+  )
+}
+
+export default ApparelPage
