dc3ea5e1cc09770bdb85686a4b38229985b4f07f
danielhincapievargas <daniel.hincapie.vargas@gmail.com>
2023-09-27 11:51:15 -0500

    sizes out of stock disabled

--- src/components/ProductDetail/index.js
+++ src/components/ProductDetail/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import styles from '@components/ProductDetail/productDetail.module.css'
 import Image from 'next/image'
 import { useSelector, useDispatch } from 'react-redux'
+import { useRouter } from 'next/router'
 import { 
   selectedProduct,
   updateProductSize,
@@ -12,17 +13,20 @@ import {
 const ProductDetail = () => {
   const selectedItem = useSelector(selectedProduct);
   const dispatch = useDispatch();
-  const [disable, setDisable] = useState(false);
+  const router = useRouter()
+  const [disableSubstract, setDisableSubstract] = useState(false);
+  const [disableAdd, setDisableAdd] = useState(false);
   const [selectedSize, setSelectedSize] = useState('');
 
   const handleAdd = () => {
-    setDisable(false)
+
+    setDisableSubstract(false)
     dispatch(incrementCount())
   }
 
   const handleSubstract = () => {
     if(selectedItem.productCount <= 1){
-      setDisable(true);
+      setDisableSubstract(true);
       return;
     } else {
       dispatch(decrementCount())
@@ -53,12 +57,18 @@ const ProductDetail = () => {
           <div className={styles.product_price}>{selectedItem.productPrice}</div>
         </div>
 
+        {(selectedItem.productType === 'Tee' || selectedItem.productType === 'Hoodie')
+        && (
         <div className={styles.sizes_container}>
           <div className={styles.sizes_title}>SIZE</div>
           <ul className={styles.sizes_list}>
             <li>
               <button 
-                className={selectedSize == 'S' ? styles.selected_size : ''}
+                className={
+                  (selectedSize === 'S' && (selectedItem.productStockS > 0)) 
+                  ? styles.selected_size 
+                  : (selectedItem.productStockS == 0)  
+                  ? styles.disabled : ''}
                 onClick={(e) => handleSize(e)}
                 value='S'
               >
@@ -67,7 +77,11 @@ const ProductDetail = () => {
             </li>
             <li>
               <button
-                className={selectedSize === 'M' ? styles.selected_size : ''}
+                className={
+                  (selectedSize === 'M' && (selectedItem.productStockM > 0))
+                  ? styles.selected_size
+                  : (selectedItem.productStockM == 0)
+                  ? styles.disabled : ''}
                 onClick={(e) => handleSize(e)}
                 value='M'
               >
@@ -76,36 +90,54 @@ const ProductDetail = () => {
             </li>
             <li>
               <button
-                className={selectedSize === 'L' ? styles.selected_size : ''}
+                className={
+                  (selectedSize === 'L' && (selectedItem.productStockL > 0)) 
+                  ? styles.selected_size 
+                  : (selectedItem.productStockL == 0)  
+                  ? styles.disabled : ''}
                 onClick={(e) => handleSize(e)}
                 value='L'
+                
               >
               L
               </button>
             </li>
             <li>
               <button
-                className={selectedSize === 'XL' ? styles.selected_size : ''}
+                className={
+                  (selectedSize === 'XL' && (selectedItem.productStockXL > 0)) 
+                  ? styles.selected_size 
+                  : (selectedItem.productStockXL == 0)  
+                  ? styles.disabled : ''}
                 onClick={(e) => handleSize(e)}
                 value='XL'
+                disabled={(selectedItem.productStockXL === 0) ? true : false}
               >
               XL
               </button>
             </li>
           </ul>
         </div>
+        )
+        }
+
 
         <div className={styles.quantity_container}>
           <div className={styles.quantity_title}>QUANTITY</div>
           <div className={styles.quantity_counter}>
-            <button disabled={disable} onClick={handleSubstract}>-</button>
+            <button disabled={disableSubstract} onClick={handleSubstract}>-</button>
             <div className={styles.count}>{selectedItem.productCount}</div>
             <button onClick={handleAdd}>+</button>
           </div>
         </div>
 
         <div className={styles.add_to_cart}>
-          <button className={styles.add_button}>ADD TO CART</button>
+          <button 
+            className={styles.add_button}
+            onClick={() => {router.push('/cart')}}
+          >
+            ADD TO CART
+          </button>
         </div>
         
       </div>
