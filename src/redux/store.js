c5dc5fc00515fc25e8b265dbebf58e8934a4bc76
danielhincapievargas <daniel.hincapie.vargas@gmail.com>
2023-09-27 22:32:57 -0500

    productSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
 import selectedProductReducer from './slices/selectedProductSlice'
 import cartReducer from './slices/cartSlice'
+import productsReducer from './slices/productSlice'
 
 const makeStore = () => configureStore({
   reducer: {
     selectedProduct: selectedProductReducer,
     cart: cartReducer,
+    products: productsReducer
   }
 })
 
