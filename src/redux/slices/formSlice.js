d46c97330289bff248a47a4f59e86afa9f521985
danielhincapievargas <daniel.hincapie.vargas@gmail.com>
2023-09-30 15:06:23 -0500

    formSlice

---
+++ src/redux/slices/formSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userEmail: '',
+  userPassword: ''
+}
+
+const formSlice = createSlice({
+  name: 'form',
+  initialState,
+  reducers: {
+    changeForm: (state, { payload }) => {
+      const {name, value} = payload
+      state.form = {
+        ...state.form,
+        [name]: value
+      }
+    },
+  }
+})
+  
+export const { changeForm } = formSlice.actions
+
+export const form = state => state.form
+
+export default formSlice.reducer
